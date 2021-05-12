696c2ac57f42179005dab921bd13e129fb7dc0b1
Jacob Thacker <>
2021-05-12 22:00:23 +0000

    Added translator to the program

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ But as of now I am(Jacob) is teaching Samuel Python and I am not exactly worried
 import string # for password gen
 import random # for ran num guesser, password gen, 8ball
 from random import randint, choice # more for rannum guesser, password gen, 8ball
+from google_trans_new import google_translator # for translator
 import sys # for the overall program
 import os # for the overall program
 import pyfiglet # just for fun
@@ -319,6 +320,42 @@ def eightballgame():
   options()
 
 
+# Translator to translate stuff I guess
+
+def googletranslator():
+	# Options
+
+	options = int(input('1: Translate\n2: See all LANGUAGES you can translate.\n(Important because you need to know the names to be able to translate..)\n3: Quit\n'))
+	translator = google_translator()
+
+	# Select the options
+	if options == 1:
+		langtgt = input('\n\nWhat language do you want it to translate the text to? \n')
+		orgin = input('\n\nWhat is the text you want to translate? \n')
+    
+		#translate code
+
+		result = translator.translate(orgin, lang_tgt=langtgt)
+		detector = google_translator()
+		detect_results = detector.detect(orgin)
+		print('\n\nTranslating....\n')
+
+		time.sleep(1)
+		
+    #translate output
+
+		print(f'The language you you just translated was: {detect_results}\nAnd translated into {langtgt} is: \n{result}\n')   
+	elif options == 2:
+
+    # Languages you can choose from
+
+		language = {'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani', 'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan', 'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)', 'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english', 'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian', 'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole', 'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian', 'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 'jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)', 'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish', 'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori', 'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto', 'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian', 'ru': 'russian', 'sm': 'samoan', 'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona', 'sd': 'sindhi', 'si': 'sinhala', 'sk': 'slovak', 'sl': 'slovenian', 'so': 'somali', 'es': 'spanish', 'su': 'sundanese', 'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik', 'ta': 'tamil', 'te': 'telugu', 'th': 'thai', 'tr': 'turkish', 'uk': 'ukrainian', 'ur': 'urdu', 'uz': 'uzbek', 'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish', 'yo': 'yoruba', 'zu': 'zulu', 'fil': 'Filipino', 'he': 'Hebrew'}
+		print(language)                                                                   
+	elif options == 3:
+		options()
+	else:
+		print('\nI\'m not sure what you mean') 
+
 
 # Greeting
 
@@ -366,10 +403,12 @@ OPTIONS:
 
 6: 8Ball Game 
 
-7: About
+7: Translator
+
+8: About
 """
 + Fore.LIGHTRED_EX + """
-8: Quit
+9: Quit
 
 """)
 
@@ -388,6 +427,8 @@ OPTIONS:
     elif mainmenu == 6:
       eightballgame()
     elif mainmenu == 7:
+      googletranslator()
+    elif mainmenu == 8:
       clear()
       input("""
 
@@ -420,14 +461,16 @@ Press enter to continue...
 
 # sad to see you go :(
       options()
-    elif mainmenu == 8:
+    elif mainmenu == 9:
       clear()
       print(f"Sad to see you go... goodbye {name} ")
       time.sleep(3)
       sys.exit()
     else:
       options()
-options()
+while True:
+  input('Press enter to continue')
+  options()
 
   
 # Should never show unless a error happened
